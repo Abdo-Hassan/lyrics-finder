b0d9788a6178867034b2f56ba485f534fd4a1e31
abdo hassan <abdocoder0@gmail.com>
2019-07-25 13:53:36 +0200

    fix font awesome + made a lyrics page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className='navbar navbar-dark bg-dark mb-5'>
-      <span className='navbar-brand mb-0 h1 mx-auto'>Lyrics</span>
+      <Link to='/' className='mx-auto'>
+        <span className='navbar-brand mb-0 h1'>Lyrics</span>
+      </Link>
     </nav>
   );
 };
--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Track = props => {
   const { track } = props;
   return (
@@ -7,6 +7,26 @@ const Track = props => {
       <div className='card mb-4 shadow-sm'>
         <div className='card-body'>
           <h5>{track.artist_name}</h5>
+          <p className='card-text'>
+            <strong>
+              <i className='fas fa-play' style={{ marginRight: 5 }} />
+              Track
+            </strong>
+            <span>{track.track_name}</span>
+            <br />
+            <strong>
+              <i className='fas fa-compact-disc' style={{ marginRight: 5 }} />
+              Album
+            </strong>
+            <span>{track.album_name}</span>
+          </p>
+          <Link
+            to={`lyrics/track/${track.track_id}`}
+            className='btn btn-block btn-dark'
+          >
+            <i className='fas fa-chevron-right' style={{ marginRight: 5 }} />
+            View Lyrics
+          </Link>
         </div>
       </div>
     </div>

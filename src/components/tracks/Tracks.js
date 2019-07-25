1812f0ed87429a6106573ca1755e869502f74ac4
abdo hassan <abdocoder0@gmail.com>
2019-07-25 13:25:50 +0200

    made a api request + spinner loading

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Consumer } from '../../context';
+import Spinner from '../layout/Spinner';
 
 const Tracks = () => {
   return (
     <Consumer>
       {value => {
-        console.log(value);
-        return <h1>Tracks</h1>;
+        const { track_list, heading } = value;
+        if (track_list === undefined || track_list.length === 0) {
+          return <Spinner />;
+        } else {
+          return (
+            <Fragment>
+              <h3 className='text-center mb-4'>{heading}</h3>
+              <div className='row' />
+            </Fragment>
+          );
+        }
       }}
     </Consumer>
   );
--- src/context.js
+++ src/context.js
@@ -1,15 +1,26 @@
 import React, { Component, createContext } from 'react';
+import axios from 'axios';
 
 const Context = createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: 'abd' } },
-      { track: { track_name: '124' } }
-    ],
+    track_list: [],
     heading: 'Top 10 Tracks'
   };
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <Context.Provider value={this.state}>

39c6c9f0171dbd296e7fa97c7755fbd52d479c4b
Abdelrahman Hassan <abdoholmes97@gmail.com>
2021-02-07 22:45:38 +0200

    removed cors https://cors-anywhere.herokuapp.com/

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -12,13 +12,13 @@ class Lyrics extends Component {
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+        `http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
         this.setState({ lyrics: res.data.message.body.lyrics });
 
         return axios.get(
-          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+          `http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
         );
       })
       .then(res => {

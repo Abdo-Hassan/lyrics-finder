88f74749b8ea88e3edef4ba1fe0023522d348fa8
Abdelrahman Hassan <abdoholmes97@gmail.com>
2021-02-08 16:24:42 +0200

    added / to endpoint

--- src/context.js
+++ src/context.js
@@ -24,7 +24,7 @@ export class Provider extends Component {
   componentDidMount() {
     axios
       .get(
-        `http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        `http://api.musixmatch.com/ws/1.1/chart.tracks.get/?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
           process.env.REACT_APP_MM_KEY
         }`
       )
